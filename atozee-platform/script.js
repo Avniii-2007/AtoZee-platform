75584d0ef17314086b7a39559048685c142c6585
Avniii-2007 <iamavniii2007@gmail.com>
2026-04-07 22:00:28 +0530

    fourth commit

--- atozee-platform/script.js
+++ atozee-platform/script.js
@@ -1,30 +1,36 @@
 let allProducts = [];
 function getProducts() {
+  let container = document.getElementById("products");
+  container.innerHTML = "<h2>Loading...</h2>";
   fetch("https://dummyjson.com/products")
-    .then(res => res.json())
-    .then(data => {
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
       allProducts = data.products;
       displayProducts(allProducts);
     })
-    .catch(err => {
+    .catch(function(err) {
       console.log(err);
-      document.getElementById("products").innerHTML =
-        "<h2>Failed to load products</h2>";
+      container.innerHTML = "<h2>Failed to load products</h2>";
     });
 }
 function displayProducts(products) {
   let container = document.getElementById("products");
   container.innerHTML = "";
 
-  products.map(p => {
+  if (products.length === 0) {
+    container.innerHTML = "<h2>No products found</h2>";
+    return;
+  }
+  products.forEach(function(p) {
     let card = document.createElement("div");
     card.className = "card";
 
-    card.innerHTML = `
-      <img src="${p.thumbnail}">
-      <h4>${p.title}</h4>
-      <p>₹ ${p.price}</p>
-    `;
+    card.innerHTML =
+      "<img src='" + p.thumbnail + "'>" +
+      "<h4>" + p.title + "</h4>" +
+      "<p>₹ " + p.price + "</p>";
 
     container.appendChild(card);
   });
@@ -32,14 +38,39 @@ function displayProducts(products) {
 function searchProduct() {
   let text = document.getElementById("searchInput").value.toLowerCase();
 
-  let filtered = allProducts.filter(p =>
-    p.title.toLowerCase().includes(text)
-  );
+  let filtered = allProducts.filter(function(p) {
+    return p.title.toLowerCase().includes(text);
+  });
 
   displayProducts(filtered);
 }
 function filterCategory(cat) {
-  let filtered = allProducts.filter(p => p.category === cat);
+  if (cat === "all") {
+    displayProducts(allProducts);
+    return;
+  }
+
+  let filtered = allProducts.filter(function(p) {
+    return p.category === cat;
+  });
+
   displayProducts(filtered);
 }
-getProducts();
+window.onload = function () {
+  getProducts();
+
+  document
+    .getElementById("searchInput")
+    .addEventListener("input", searchProduct);
+};
+function toggleMode() {
+  document.body.classList.toggle("dark-mode");
+
+  let btn = document.getElementById("modeBtn");
+
+  if (document.body.classList.contains("dark-mode")) {
+    btn.innerText = "Mode☀️";
+  } else {
+    btn.innerText = "Mode🌙";
+  }
+}
